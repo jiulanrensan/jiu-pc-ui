8b6f4a067ae9598c956bb2aa4b16ccbf6a50915c
jiulanrensan <839492148@qq.com>
2020-07-02 10:51:07 +0800

    feat(components/button/src,src/views/button): 增加button组件，添加type,size,round,disabled功能

--- components/button/src/button.vue
+++ components/button/src/button.vue
@@ -1,17 +1,164 @@
 <template>
-  <button class="j-button">
-    <span>button</span>
+  <button
+    class="j-button"
+    :disabled="disabled"
+    :class="[
+      type ? 'j-button-' + type : '',
+      size ? 'j-button-' + size : '',
+      round ? 'j-button-round-' + size : '',
+      disabled ? 'j-button-disabled' : ''
+    ]">
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 
 <script>
 export default {
-  name: 'JButton'
+  name: 'JButton',
+  props: {
+    size: {
+      type: String,
+      // small, medium, large
+      default: 'medium'
+    },
+    type: {
+      type: String,
+      // default, primary, success, warning, danger, info
+      default: 'default'
+    },
+    // 是否圆角
+    round: Boolean,
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+  }
 }
 </script>
 
 <style lang="scss">
+  @import "../../style/var.scss";
+  @mixin button-color ($color, $bg-color, $bd-color){
+    color: $color;
+    background-color: $bg-color;
+    border-color: $bd-color;
+    &:hover,
+    &:focus {
+      background: mix($--color-default, $bg-color, 20%);
+      border-color: mix($--color-default, $bd-color, 20%);
+      color: $color;
+    }
+    &:active {
+      background: mix($--color-black, $bg-color, 10%);
+      border-color: mix($--color-black, $bd-color, 10%);
+      color: $color;
+    }
+    &.j-button-disabled {
+      &,
+      &:hover,
+      &:focus,
+      &:active {
+        cursor: not-allowed;
+        background-color: mix($bg-color, $--color-default);
+        border-color: mix($bd-color, $--color-default);
+      }
+    }
+  }
+  @mixin button-round-cal ($type) {
+    border-radius: $--border-width-default + $--font-line-height-default/2 + $type;
+  }
+  @mixin button-padding-cal ($type) {
+    padding: $type $type*2;
+  }
   .j-button{
-    
+    box-sizing: border-box;
+    padding: $--button-padding-medium $--button-padding-medium*2;
+    background: $--color-default;
+    color: $--color-text-default;
+    border: $--border-width-default $--border-style-default $--border-color-default;
+    border-radius: $--border-radius-default;
+    line-height: $--font-line-height-default;
+    outline: none;
+    transition: all 0.1s;
+    cursor: pointer;
+  }
+  .j-button-small{
+    // @include button-padding-cal(small);
+    @include button-padding-cal($--button-padding-small);
+  }
+  .j-button-medium{
+    @include button-padding-cal($--button-padding-medium);
+  }
+  .j-button-large{
+    @include button-padding-cal($--button-padding-large);
+  }
+  .j-button-default{
+    &:hover,
+    &:focus{
+      background: mix($--color-default, $--color-primary, 90%);
+      border-color: mix($--color-default, $--color-primary, 90%);
+      color: mix($--color-default, $--color-primary, 10%);
+    }
+    &:active {
+      background: mix($--color-default, $--color-primary, 90%);
+      border-color: mix($--color-black, $--color-primary, 10%);
+      color: mix($--color-default, $--color-primary, 10%);
+    }
+    &.j-button-disabled {
+      &,
+      &:hover,
+      &:focus,
+      &:active {
+        cursor: not-allowed;
+        background: $--color-default;
+        border-color: mix($--color-default, $--color-primary, 90%);
+        color: mix($--color-default, $--color-text-default, 10%);
+      }
+    }
+  }
+  .j-button-primary{
+    @include button-color($--color-text-white, $--color-primary, $--color-primary);
+  }
+  .j-button-success{
+    @include button-color($--color-text-white, $--color-success, $--color-success);
+  }
+  .j-button-warning{
+    @include button-color($--color-text-white, $--color-warning, $--color-warning);
+  }
+  .j-button-danger{
+    @include button-color($--color-text-white, $--color-danger, $--color-danger);
+  }
+  .j-button-info{
+    @include button-color($--color-text-white, $--color-info, $--color-info);
+  }
+  .j-button-text{
+    color: mix($--color-default, $--color-text-default, 60%);
+    background-color: $--color-default;
+    border-color: transparent;
+    &:active {
+      color: mix($--color-default, $--color-text-default, 10%);
+    }
+    &.j-button-disabled {
+      &,
+      &:hover,
+      &:focus,
+      &:active {
+        cursor: not-allowed;
+      }
+    }
+  }
+  // 需要用函数
+  .j-button-round-small{
+    @include button-round-cal($--button-padding-small);
+  }
+  .j-button-round-medium{
+    @include button-round-cal($--button-padding-medium);
+  }
+  .j-button-round-large{
+    @include button-round-cal($--button-padding-large);
   }
 </style>
--- src/views/button.vue
+++ src/views/button.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <p>use JButton</p>
-    <j-button></j-button>
+    <j-button>点击</j-button>
+    <j-button type="primary">点击</j-button>
+    <j-button type="success">点击</j-button>
+    <j-button type="warning">点击</j-button>
+    <j-button type="danger">点击</j-button>
+    <j-button type="info">点击</j-button>
+    <br>
+    <br>
+    <j-button round>点击</j-button>
+    <j-button type="primary" round>点击</j-button>
+    <j-button type="success" round>点击</j-button>
+    <j-button type="warning" round>点击</j-button>
+    <j-button type="danger" round>点击</j-button>
+    <j-button type="info" round>点击</j-button>
+    <br>
+    <br>
+    <j-button size="small">点击</j-button>
+    <j-button size="medium">点击</j-button>
+    <j-button size="large">点击</j-button>
+    <br>
+    <br>
+    <j-button type="text">点击</j-button>
+    <br>
+    <br>
+    <j-button type="text" disabled>点击</j-button>
+    <j-button disabled>点击</j-button>
+    <j-button type="primary" disabled>点击</j-button>
+    <j-button type="success" disabled>点击</j-button>
+    <j-button type="warning" disabled>点击</j-button>
+    <j-button type="danger" disabled>点击</j-button>
+    <j-button type="info" disabled>点击</j-button>
   </div>
 </template>
 
