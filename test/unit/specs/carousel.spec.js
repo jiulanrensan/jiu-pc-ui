bd162291222449d4ec94de87f8e82ae47db5d53c
李嘉城 <839492148@qq.com>
2020-07-16 11:09:31 +0800

    fix(test): 下载karma-chrome-launcher;测试用例修改、学习

--- test/unit/specs/carousel.spec.js
+++ test/unit/specs/carousel.spec.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import carousel from '@view/carousel'
+import carousel from 'package/carousel/src'
+import button from '@/views/button'
 
-describe('carousel.vue', () => {
+describe('button.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(carousel)
+    const Constructor = Vue.extend(button)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.j-carousel-container').className)
-      .to.equal('j-carousel-container')
+    expect(vm.$el.querySelector('p').textContent)
+      .to.equal('use JButton')
   })
 })
